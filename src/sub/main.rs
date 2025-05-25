86492e0f6512a67ca019a396b4aed9b2eec20101
alazani <flatoutta@gmail.com>
2025-05-26 08:35:06 +0900

    作業中

---
+++ src/sub/main.rs
@@ -0,0 +1,44 @@
+
+fn subscribe_topics(cli: &mqtt::Client) {
+    if let Err(e) = cli.subscribe_many(DFLT_TOPICS, DFLT_QOS) {
+        println!("Error subscribes topics: {:?}", e);
+        process::exit(1);
+    }
+
+}fn main() {
+    const DFLT_BROKER:&str = "tcp://broker.emqx.io:1883";
+    const DFLT_CLIENT:&str = "rust_publish";
+    const DFLT_TOPICS:&[&str] = &["rust/mqtt", "rust/test"];
+
+    // println!("Hello, world!");
+    let host = env::args().nth(1).unwrap_or_else(||
+        DFLT_BROKER.to_string()
+    );
+
+    // Define the set of options for the create.
+    // Use an ID for a persistent session.
+    let create_opts = mqtt::CreateOptionsBuilder::new()
+        .server_uri(host)
+        .client_id(DFLT_CLIENT.to_string())
+        .finalize();
+
+    // Create a client.
+    let cli = mqtt::Client::new(create_opts).unwrap_or_else(|err| {
+        println!("Error creating the client: {:?}", err);
+        process::exit(1);
+    });
+
+    // Define the set of options for the connection.
+    let conn_opts = mqtt::ConnectOptionsBuilder::new()
+        .keep_alive_interval(Duration::from_secs(20))
+        .clean_session(true)
+        .finalize();
+
+    // Connect and wait for it to complete or fail.
+    if let Err(e) = cli.connect(conn_opts) {
+        println!("Unable to connect:\n\t{:?}", e);
+        process::exit(1);
+    }
+
+}
+
